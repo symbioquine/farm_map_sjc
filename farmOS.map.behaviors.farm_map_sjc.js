12a6e253e0217bc183d5d7d48905bcde17d01dd4
Symbioquine <symbioquine@gmail.com>
2020-04-04 07:51:16 -0700

    Add all sjc base layers

--- farmOS.map.behaviors.farm_map_sjc.js
+++ farmOS.map.behaviors.farm_map_sjc.js
@@ -2,17 +2,39 @@
   farmOS.map.behaviors.farm_map_sjc = {
     attach: function (instance) {
 
-      // Add San Juan Islands Aerials
-      var opts = {
-        title: 'SJC Aerials',
-        url: 'https://myexample.farm/service',
-        params: {
-          LAYERS: 'Image',
-          VERSION: '1.3.0',
-        },
-        group: 'Base layers',
-      };
-      instance.addLayer('wms', opts);
+      // From https://gis.sanjuanco.com/arcgis/rest/services/Basemaps?f=pjson
+      var basemaps = [
+        { "name": "Basemaps/Aerials_1932", "type": "MapServer" },
+        { "name": "Basemaps/Aerials_2004", "type": "MapServer" },
+        { "name": "Basemaps/Aerials_2006", "type": "MapServer" },
+        { "name": "Basemaps/Aerials_2008", "type": "MapServer" },
+        { "name": "Basemaps/Aerials_2013_WM", "type": "MapServer" },
+        { "name": "Basemaps/Aerials_2013", "type": "MapServer" },
+        { "name": "Basemaps/Aerials_2016_WM", "type": "MapServer" },
+        { "name": "Basemaps/Aerials_2016", "type": "MapServer" },
+        { "name": "Basemaps/Aerials_2019_WM", "type": "MapServer" },
+        { "name": "Basemaps/Aerials_2019", "type": "MapServer" },
+        { "name": "Basemaps/General_Basemap_WM_nostatelands", "type": "MapServer" },
+        { "name": "Basemaps/General_Basemap_WM", "type": "MapServer" },
+        { "name": "Basemaps/General_Basemap", "type": "MapServer" },
+        { "name": "Basemaps/Reference_Overlay_WM", "type": "MapServer" },
+        { "name": "Basemaps/Reference_Overlay", "type": "MapServer" },
+        { "name": "Basemaps/Terrain_Basemap_General", "type": "MapServer" },
+        { "name": "Basemaps/Terrain_Basemap", "type": "MapServer"}
+      ];
+
+      basemaps.forEach(function(basemap) {
+        // Add San Juan Islands Basemaps
+        var opts = {
+          title: "SJC " + basemap.name.split('/')[1],
+          url: 'https://gis.sanjuanco.com/arcgis/rest/services/' + basemap.name + '/' + basemap.type,
+          visible: false,
+          base: true,
+          group: 'Base layers',
+        };
+        instance.addLayer('arcgis-tile', opts);
+      });
+
     },
   };
 }());
