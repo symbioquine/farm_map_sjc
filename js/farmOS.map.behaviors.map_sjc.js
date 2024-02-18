d2c404a49b1172aa3e4502f28e3447f2ab68233c
Symbioquine <symbioquine@gmail.com>
2024-02-18 09:31:32 -0800

    Update urls to use new domain and add additional imagery layers

--- js/farmOS.map.behaviors.map_sjc.js
+++ js/farmOS.map.behaviors.map_sjc.js
@@ -2,8 +2,8 @@
   farmOS.map.behaviors.map_sjc = {
     attach: function (instance) {
 
-      // From https://gis.sanjuanco.com/arcgis/rest/services/Basemaps?f=pjson
       var basemaps = [
+        // From https://gis.sanjuancountywa.gov/arcgis/rest/services/Basemaps?f=json
         { "name": "Basemaps/Aerials_1932", "type": "MapServer" },
         { "name": "Basemaps/Aerials_2004", "type": "MapServer" },
         { "name": "Basemaps/Aerials_2006", "type": "MapServer" },
@@ -21,14 +21,19 @@
         { "name": "Basemaps/Reference_Overlay_WM", "type": "MapServer" },
         { "name": "Basemaps/Reference_Overlay", "type": "MapServer" },
         { "name": "Basemaps/Terrain_Basemap_General", "type": "MapServer" },
-        { "name": "Basemaps/Terrain_Basemap", "type": "MapServer"}
+        { "name": "Basemaps/Terrain_Basemap", "type": "MapServer"},
+        // From https://gis.sanjuancountywa.gov/arcgis/rest/services/Imagery?f=json
+        { "name": "Imagery/Bare_Earth_2009", "type": "MapServer" },
+        { "name": "Imagery/Bare_Earth_2013", "type": "MapServer" },
+        { "name": "Imagery/Bare_Earth_2019", "type": "MapServer" },
+        { "name": "Imagery/SJC_VegHeight_2019", "type": "MapServer"},
       ];
 
       basemaps.forEach(function(basemap) {
         // Add San Juan Islands Basemaps
         var opts = {
           title: "SJC " + basemap.name.split('/')[1],
-          url: 'https://gis.sanjuanco.com/arcgis/rest/services/' + basemap.name + '/' + basemap.type,
+          url: 'https://gis.sanjuancountywa.gov/arcgis/rest/services/' + basemap.name + '/' + basemap.type,
           visible: false,
           base: true,
           group: 'Base layers',
